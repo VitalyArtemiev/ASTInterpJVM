0bd1d80d7219a7105b4543507e3bc12e8866c77d
Виталий Артемьев <vitaly.artemiev@yandex.ru>
2020-02-15 02:19:15 +0300

    Finished Optimizer Apparently fixed issue with inlining functions with side-effects, will see Really need to think about how to refactor

--- src/main/kotlin/interpreter/Optimizer.kt
+++ src/main/kotlin/interpreter/Optimizer.kt
@@ -40,6 +40,10 @@ class Optimizer (val r: Runner) {
         return env
     }
 
+    /*fun getEnv(): Environment {
+        return Environment(root, constTable.clone(), varTable, functions)
+    }*/
+
     fun printVarTable() {
         println("${util.PURPLE}Variables: ${util.CYAN}")
         for (v in varTable) {
@@ -54,11 +58,22 @@ class Optimizer (val r: Runner) {
         println(util.RESET)
     }
 
-    fun evalExpr(node: Expr, constOnly: Boolean): Any {
+    var canConst = true
+
+    fun tryConst(node: ASTNode, const: ConstVal ): ASTNode {
+        return if (canConst) {
+            const
+        } else {
+            node
+        }
+    }
+
+    fun optimizeExpr(node: Expr, constOnly: Boolean): Any {
         when (node) {
             is UnOp -> {
-                val right = evalExpr(node.right, constOnly)
-                node.right = ConstVal(right, getExprType(right), node.right.token)
+                val right = optimizeExpr(node.right, constOnly)
+                if (canConst) node.right = ConstVal(right, getExprType(right), node.right.token)
+                canConst = true
 
                 return when (node.op) {
                     TokenTypeEnum.unaryMinusOp -> {- right}
@@ -68,11 +83,13 @@ class Optimizer (val r: Runner) {
                 }
             }
             is BinOp -> {
-                var left = evalExpr(node.left, constOnly)
-                node.left = ConstVal(left, getExprType(left), node.left.token)
+                var left = optimizeExpr(node.left, constOnly)
+                if (canConst) node.left = ConstVal(left, getExprType(left), node.left.token)
+                canConst = true
 
-                var right = evalExpr(node.right, constOnly)
-                node.right = ConstVal(right, getExprType(right), node.right.token)
+                var right = optimizeExpr(node.right, constOnly)
+                if (canConst) node.right = ConstVal(right, getExprType(right), node.right.token)
+                canConst = true
 
                 if (node.op in relOps) {
                     if (left is Float && right is Int) {
@@ -118,7 +135,13 @@ class Optimizer (val r: Runner) {
                 return node.value
             }
             is Call -> {
-                return executeCall(node, constOnly).value ?: throw OptimizerException("Call execution ($node) returned no value")
+                return if (node.callee.body is Block && !hasSideEffects(node.callee.body as Block)) {
+                    optimizeCall(node, constOnly).value ?: throw OptimizerException("Call execution ($node) returned no value")
+                } else {
+                    canConst = false
+                    executeCall(node).value ?: throw OptimizerException("Call execution ($node) returned no value")
+                }
+                //todo: need to consider side effects
             }
             else -> {
                 throw OptimizerException("Expression evaluation encountered unsupported node: $node")
@@ -209,15 +232,19 @@ class Optimizer (val r: Runner) {
             }
 
             is Expr -> {
-                val result = evalExpr(node, constOnly)
+                val result = optimizeExpr(node, constOnly)
                 val type = getExprType(result)
                 return ExecutionResult(type, result)
             }
             is Call -> {
-                return executeCall(node, constOnly)
+                return if (node.callee.body is Block && !hasSideEffects(node.callee.body as Block)) {
+                    optimizeCall(node, constOnly)
+                } else {
+                    executeCall(node)
+                }
             }
             is CallStmt -> {
-                return executeCall(node.call, constOnly)
+                return optimizeCall(node.call, constOnly)
             }
             is VarDecl -> {
                 if (node.expr != null) {
@@ -237,7 +264,8 @@ class Optimizer (val r: Runner) {
                 val result = optimizeNode(node.expr, constOnly)
                 require(result.type == varTable[node.identifier.refId].type)//todo: are these checks needed
                 {"Assign type error: expected ${varTable[node.identifier.refId].type}, got ${result.type}"}
-                node.expr = ConstVal(result.value!!, result.type, node.expr.token)
+                if (canConst) node.expr = ConstVal(result.value!!, result.type, node.expr.token)
+                canConst = true
 
                 varTable[node.identifier.refId].value = result.value
                 assignStack.pop()
@@ -248,7 +276,8 @@ class Optimizer (val r: Runner) {
                 require(result.type == ValType.bool)//todo: are these checks needed
                 {"Condition type error: expected bool, got ${result.type} "}
 
-                node.condition = ConstVal(result.value!!, result.type, node.condition.token)
+                if (canConst) node.condition = ConstVal(result.value!!, result.type, node.condition.token)
+                canConst = true
 
                 return when (result.value as Boolean) {
                     true -> { optimizeNode(node.s, constOnly) }
@@ -257,7 +286,7 @@ class Optimizer (val r: Runner) {
             }
             is While -> {
                 try {
-                    var result = optimizeNode(node.condition, true)
+                    var result = optimizeNode(node.condition, constOnly = true)
                     require(result.type == ValType.bool)
                     { "Condition type error: expected bool, got ${result.type} " }
                 } catch (e: OptimizerException) {
@@ -287,7 +316,7 @@ class Optimizer (val r: Runner) {
         }
     }
 
-    fun executeCall(node: Call, constOnly: Boolean): ExecutionResult {
+    fun optimizeCall(node: Call, constOnly: Boolean): ExecutionResult {
         callStack.push(node.callee.identifier.refId)
         val result = when (node.callee.body) {
             is Block -> {
@@ -302,10 +331,243 @@ class Optimizer (val r: Runner) {
                     varTable[signature[i].identifier.refId].value = result.value //init param variables
                 }
 
-                optimizeNode(node.callee.body, constOnly)
+                if (hasSideEffects(node.callee.body as Block)) {
+                    try {
+                        optimizeNode(node.callee.body, constOnly = true)
+                    } catch (e: OptimizerException) {
+                        //blockStack.pop()
+                        logger.i("Encountered var in function with side effects; leaving")
+                        executeNode(node.callee.body)
+                    }
+                } else {
+                    optimizeNode(node.callee.body, constOnly)
+                }
             } //execute function body block
             is PrecompiledBlock -> {
                 ExecutionResult(node.callee.retType, null)
+            }
+        }
+        callStack.pop()
+        return result
+    }
+
+    fun hasSideEffects(node: Block): Boolean {
+        for (n in node.nodes) {
+            when (n) {
+                is Assign -> {
+                    if (n.identifier.scopeIndex != node.scopeIndex) {
+                        return true
+                    }
+                }
+                is Call -> {
+                    return true //todo: implement recursive checks via passable stack
+                }
+            }
+        }
+        return false
+    }
+
+    fun evalExpr(node: Expr): Any {
+        when (node) {
+            is UnOp -> {
+                val right = evalExpr(node.right)
+
+                return when (node.op) {
+                    TokenTypeEnum.unaryMinusOp -> {- right}
+                    TokenTypeEnum.unaryPlusOp -> {right}
+                    TokenTypeEnum.notOp -> {! right}
+                    else -> {throw RunnerException("Expression evaluation encountered unsupported operator: ${node.op}")}
+                }
+            }
+            is BinOp -> {
+                var left = evalExpr(node.left)
+                var right = evalExpr(node.right)
+
+                if (node.op in relOps) {
+                    if (left is Float && right is Int) {
+                        right = right.toFloat()
+                    } else if (left is Int && right is Float) {
+                        left = left.toFloat()
+                    }
+                }
+
+                val result = when (node.op) {
+                    TokenTypeEnum.plusOP -> {left + right}
+                    TokenTypeEnum.minusOp -> {left - right}
+                    TokenTypeEnum.divOp -> {left / right}
+                    TokenTypeEnum.multOp -> {left * right}
+                    TokenTypeEnum.powOp -> {left pow right}
+
+                    TokenTypeEnum.andOp -> {left and right}
+                    TokenTypeEnum.orOP -> {left or right}
+                    TokenTypeEnum.xorOp -> {left xor right}
+
+                    TokenTypeEnum.equal -> {left as Comparable<Any> == right} //todo: check if this comparable trick works
+                    TokenTypeEnum.less -> {left as Comparable<Any> < right}
+                    TokenTypeEnum.greater -> {left as Comparable<Any> > right}
+                    TokenTypeEnum.notEqual -> {left as Comparable<Any> != right}
+                    TokenTypeEnum.lequal -> {left as Comparable<Any> <= right}
+                    TokenTypeEnum.gequal -> {left as Comparable<Any> >= right}
+                    else -> {throw RunnerException("Expression evaluation encountered unsupported operator: $node")}
+                }
+
+                return result
+            }
+            is VarRef -> {
+                return varTable[node.varId].value ?:
+                throw RunnerException("Referencing uninitialised variable ${varTable[node.varId]} at $node")
+            }
+            is ConstRef -> {
+                return constTable[node.constId].value
+            }
+            is ConstVal -> {
+                return node.value
+            }
+            is Call -> {
+                return executeCall(node).value ?: throw RunnerException("Call execution ($node) returned no value")
+            }
+            else -> {
+                throw RunnerException("Expression evaluation encountered unsupported node: $node")
+            }
+        }
+    }
+
+    fun executeNode(node: ASTNode): ExecutionResult {
+        when (node) {
+            is Prog -> {
+                for (n in node.nodes) {
+                    executeNode(n)
+                }
+                return ExecutionResult(ValType.none, null)
+            }
+            is Block -> {
+                if (node.nodes.size == 0) {
+                    logger.w("Runner encountered empty block at $node")
+                }
+
+                for (n in node.nodes) {
+                    if (n is Return) {
+                        return executeNode(n)
+                    } else {
+                        executeNode(n)
+                    }
+                }
+
+                return ExecutionResult(ValType.none, null)
+            }
+
+            is Expr  -> {
+                val result = evalExpr(node)
+                val type = when (result) {
+                    is Int -> ValType.int
+                    is Float, is Double -> ValType.float
+                    is Boolean -> ValType.bool
+                    else -> {
+                        logger.e("Execution expected Expr, received $result")
+                        ValType.none
+                    }
+                }
+                return ExecutionResult(type, result)
+            }
+            is Call  -> {
+                return executeCall(node)
+            }
+            is CallStmt -> {
+                return executeCall(node.call)
+            }
+            is VarDecl -> {
+                if (node.expr != null) {
+                    val result = executeNode(node.expr as Expr)
+                    require(result.type == varTable[node.identifier.refId].type)
+                    {"Declaration assignment type error: expected ${varTable[node.identifier.refId].type}, got ${result.type}"}
+                    varTable[node.identifier.refId].value = result.value
+                }
+                return ExecutionResult(ValType.none, null)
+            }
+            is ConstDecl, is FunDecl -> {
+                //todo: hmm
+                return ExecutionResult(ValType.none, null)
+            }
+            is Assign -> {
+                val result = executeNode(node.expr)
+                require(result.type == varTable[node.identifier.refId].type)//todo: are these checks needed
+                {"Assign type error: expected ${varTable[node.identifier.refId].type}, got ${result.type}"}
+
+                varTable[node.identifier.refId].value = result.value
+                return ExecutionResult(ValType.none, null)
+            }
+            is If -> {
+                val result = executeNode(node.condition)
+                require(result.type == ValType.bool)//todo: are these checks needed
+                {"Condition type error: expected bool, got ${result.type} "}
+
+                return when (result.value as Boolean) {
+                    true -> { executeNode(node.s) }
+                    false -> { ExecutionResult(ValType.none, null) }
+                }
+            }
+            is While -> {
+                var result = executeNode(node.condition)
+                require(result.type == ValType.bool)
+                {"Condition type error: expected bool, got ${result.type} "}
+                while (result.value as Boolean) {
+                    executeNode(node.s)
+                    result = executeNode(node.condition)
+                }
+                return ExecutionResult(ValType.none, null)
+            }
+            is Return -> {
+                var result = executeNode(node.e)
+                require(result.type == functions[callStack.peek()].retType)
+                {"Return type error: expected ${functions[callStack.peek()].retType}, got ${result.type}"}
+                return result
+            }
+            else -> {
+                throw RunnerException("Execution encountered unsupported node: $node")
+            }
+        }
+    }
+
+    fun executeCall(node: Call): ExecutionResult {
+        callStack.push(node.callee.identifier.refId)
+        val result = when (node.callee.body) {
+            is Block -> {
+                val signature = node.callee.params
+                for ((i, p) in node.params.withIndex()) {
+                    val result = executeNode(p) //get param values
+                    if (signature[i].type != result.type) { //check param types
+                        throw RunnerException("Function parameter type mismatch: expected ${signature[i].type}, " +
+                                " got $result")
+                    }
+
+                    varTable[signature[i].identifier.refId].value = result.value //init param variables
+                }
+
+                executeNode(node.callee.body)
+            } //execute function body block
+            is PrecompiledBlock -> {
+                val signature = node.callee.params
+                val parResults = Params(node.callee.params.size) {null}
+                for ((i, p) in node.params.withIndex()) {
+                    val result = executeNode(p) //get param values
+                    if (signature[i].type != result.type && signature[i].type != ValType.any) { //check param types
+                        throw RunnerException("Precompiled function parameter type mismatch: expected ${signature[i].type}," +
+                                " got ${result.type}")
+                    }
+
+                    parResults[i] = result.value //init param variables
+                }
+
+                val result = (node.callee.body as PrecompiledBlock).f(parResults)
+                val type: ValType = when(result) {
+                    is Int -> {ValType.int}
+                    is Float -> {ValType.float}
+                    is Boolean -> {ValType.bool}
+                    is Unit -> {return ExecutionResult(ValType.none, null)}
+                    else -> {throw RunnerException("Unsupported return type from precompiled function: $result")}
+                }
+
+                ExecutionResult(type, result)
             }
         }
         callStack.pop()
